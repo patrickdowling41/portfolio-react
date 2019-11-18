29ab30a1473aec6ef5961b211a19eb269e7c3754
rmit-s3605032-patrick-dowling <s3605032@student.rmit.edu.au>
2019-11-18 21:36:12 +1100

    Added basic logic for Nav (not working yet)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,19 +3,77 @@ import Nav from './components/nav/Nav'
 import Home from './components/home/Home'
 import Projects from './components/projects/Projects'
 import Footer from './components/footer/Footer'
+import './styles/app.css'
 
 // This class is responsible for containing all other components.
 class App extends Component {
-    constructor(props)
+    constructor()
     {
-        super(props)
+        super()
+        this.state = {
+            scrollHeight: 0,
+            /* nav state options:
+            ** initial (the standard nav that appears atop the page)
+            ** transparent (when user is scrolling up from down the page)
+            ** invisible */
+            navState: 'initial'
+        }
+        this.componentDidMount = this.componentDidMount.bind(this)
+        this.componentWillUnmount = this.componentWillUnmount.bind(this)
+        this.setNav = this.setNav.bind(this)
+    }
+
+    setNav()
+    {
+
+        const newNavState = this.state.navState
+
+        // Used to determine when initial page has been scrolled past.
+        if (window.scrollY > 50) {
+            // Used to work out if the user scrolled up or down.
+            if (window.scrollY > this.state.scrollHeight)
+            {
+                this.setState({navState: "invisible"})
+            }
+            else
+            {
+                // Show transparent nav when user scrolls up.
+                this.setState({navState: "transparent"})
+            }
+        }
+        else
+        {
+            this.setState({navState: "initial"})
+        }
+        this.setState({
+            navState: newNavState,
+            scrollHeight: window.scrollY                 
+        })
+
+        console.log(this.state.navState)
+    }
+
+    componentDidMount()
+    {
+        window.addEventListener('scroll', () => 
+        {
+            this.setNav()
+        })
+    }
+
+    componentWillUnmount()
+    {
+        window.removeEventListener('scroll')
     }
 
     render() {
         return (
             <div className="container">
                 <div className="nav-container">
-                    <Nav></Nav>
+                    <Nav
+                        navState = {this.state.navState}
+                    >
+                    </Nav>
                 </div>
                 <div className="home-container">
                     <Home></Home>
--- portfolio/src/components/nav/Nav.js
+++ portfolio/src/components/nav/Nav.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
+import '../../styles/nav.css'
 
 class Nav extends Component {
-    render() {
-        return (
-            <>
-                
-            </>
-        )
+
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            navState: this.props.navState
+        }
     }
 
+    render() {
+        return (
+            <div className="container">
+                <div className={"nav-" + this.state.navState}/>
+            </div>
+        )
+    }
 }
 export default Nav
