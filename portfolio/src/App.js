c9499112bb599db3b5c43c847b09606a7cb5884b
rmit-s3605032-patrick-dowling <s3605032@student.rmit.edu.au>
2019-11-19 21:38:57 +1100

    Nav transitions working, not styled

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -10,69 +10,13 @@ class App extends Component {
     constructor()
     {
         super()
-        this.state = {
-            scrollHeight: 0,
-            /* nav state options:
-            ** initial (the standard nav that appears atop the page)
-            ** transparent (when user is scrolling up from down the page)
-            ** invisible */
-            navState: 'initial'
-        }
-        this.componentDidMount = this.componentDidMount.bind(this)
-        this.componentWillUnmount = this.componentWillUnmount.bind(this)
-        this.setNav = this.setNav.bind(this)
-    }
-
-    setNav()
-    {
-
-        const newNavState = this.state.navState
-
-        // Used to determine when initial page has been scrolled past.
-        if (window.scrollY > 50) {
-            // Used to work out if the user scrolled up or down.
-            if (window.scrollY > this.state.scrollHeight)
-            {
-                this.setState({navState: "invisible"})
-            }
-            else
-            {
-                // Show transparent nav when user scrolls up.
-                this.setState({navState: "transparent"})
-            }
-        }
-        else
-        {
-            this.setState({navState: "initial"})
-        }
-        this.setState({
-            navState: newNavState,
-            scrollHeight: window.scrollY                 
-        })
-
-        console.log(this.state.navState)
-    }
-
-    componentDidMount()
-    {
-        window.addEventListener('scroll', () => 
-        {
-            this.setNav()
-        })
-    }
-
-    componentWillUnmount()
-    {
-        window.removeEventListener('scroll')
     }
 
     render() {
         return (
             <div className="container">
                 <div className="nav-container">
-                    <Nav
-                        navState = {this.state.navState}
-                    >
+                    <Nav>
                     </Nav>
                 </div>
                 <div className="home-container">
--- portfolio/src/components/nav/Nav.js
+++ portfolio/src/components/nav/Nav.js
@@ -7,8 +7,63 @@ class Nav extends Component {
     {
         super(props)
         this.state = {
-            navState: this.props.navState
+            scrollHeight: 0,
+            /* nav state options:
+            ** initial (the standard nav that appears atop the page)
+            ** transparent (when user is scrolling up from down the page)
+            ** invisible */
+            navState: 'initial'
         }
+
+        this.componentDidMount = this.componentDidMount.bind(this)
+        this.componentWillUnmount = this.componentWillUnmount.bind(this)
+        this.setNav = this.setNav.bind(this)
+    }
+
+    setNav()
+    {
+
+        console.log("before " + this.state.scrollHeight)
+            console.log("after  " + window.scrollY)
+
+        // Used to determine when initial page has been scrolled past.
+        if (window.scrollY > 150) {
+            // Used to work out if the user scrolled up or down.
+            
+
+            if (window.scrollY > this.state.scrollHeight)
+            {
+                this.setState({navState: "invisible"})
+            }
+            else
+            {
+                // Show transparent nav when user scrolls up.
+                this.setState({navState: "transparent"})
+            }
+        }
+        else
+        {
+            this.setState({navState: "initial"})
+        }
+        this.setState({
+            scrollHeight: window.scrollY
+        })
+
+        console.log(this.state.navState)
+
+    }
+
+    componentDidMount()
+    {
+        window.addEventListener('scroll', () => 
+        {
+            this.setNav()
+        })
+    }
+
+    componentWillUnmount()
+    {
+        window.removeEventListener('scroll')
     }
 
     render() {
