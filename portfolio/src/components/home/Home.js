a1e6b5c02cc9fcba04d2976089e6faf2ab6e1479
Patrick Dowling <patrick@livit.com.au>
2019-11-26 18:36:02 +1100

    Home working, needs proper integration with nav

--- portfolio/src/components/home/Home.js
+++ portfolio/src/components/home/Home.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
+import '../../styles/home.scss'
 
 class Home extends Component {
     render() {
         return (
-            <>
-            </>
+            <div className="name-container">
+                <div className="vert-box"><h1>Patrick Dowling</h1></div>
+                <div className="vert-box"><h2>Bachelor of Software Engineering</h2></div>
+            </div>
         )
     }
 
