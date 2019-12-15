56961d8acb9c6cd49b096d15a88923bfe1d46489
rmit-s3605032-patrick-dowling <s3605032@student.rmit.edu.au>
2019-12-15 23:09:35 +1100

    Added formik import and base structure

--- portfolio/src/components/contactme/ContactMe.js
+++ portfolio/src/components/contactme/ContactMe.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react'
+import { Formik, Field, Form} from 'formik'
 
-class Footer extends Component {
+class ContactMe extends Component {
     render() {
         return (
             <>
+                <h2 className="contact-header">
+                    If you have any questions, send me through a message
+                </h2>
+                <Formik
+                    initialValues={{
+                        name: "",
+                        email: "",
+                        phoneNo: "",
+                        message: ""
+                    }}
+                >
+
+                </Formik>
             </>
         )
     }
 
 }
-export default Footer
+export default ContactMe
