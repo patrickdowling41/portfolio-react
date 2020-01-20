e051b176cc6d316dbd3558ee85f1aec7cd0a8621
Patrick Dowling <patrick@livit.com.au>
2020-01-20 14:52:37 +1100

    Added social media / email icons to hojme

--- portfolio/src/components/home/Home.js
+++ portfolio/src/components/home/Home.js
@@ -9,9 +9,11 @@ class Home extends Component {
             <div className="home-container">
                 <div className="vert-box"><h1 className="homeHeading">Patrick Dowling</h1></div>
                 <div className="vert-box"><h2 className="homeTitle">Software Engineer</h2></div>
-                {/* <div className="socialmedia-links"> */}
-                <FontAwesome name="github" />
-                {/* </div */}
+                <div className="socialmedia-links">
+                    <FontAwesome size='2x' className="sm-icon" name="linkedin" />
+                    <FontAwesome size='2x' className="sm-icon" name="envelope" />
+                    <FontAwesome size='2x' className="sm-icon" name="github" />
+                </div>
             </div>
         )
     }
